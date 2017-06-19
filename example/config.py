d7ba28fb8522165a64c8fe23b2b760beb4079d0d
antikytheraton <binauralvoice@gmail.com>
2017-06-19 15:23:44 -0500

    lol

--- example/config.py
+++ example/config.py
@@ -1,5 +1,5 @@
 CONFIG = {
-    'FACEBOOK_TOKEN': 'EAAbrMRr7dV0BAKA7PaeZAmkGrxDMC0bedZCwoM6xaKe8oqW27SrC2eo44vNZAFuFRutP0DvUbWpegZBFJs0bP2Ir1dr0bSTnFtfV3xmHTWzvTwNCNFyOZB4SW3UwZC4vF6cDN4srb9FL7nwGDbuwqZArT12Bhu4LRVq0KfaH4kjwgZDZD',
+    'FACEBOOK_TOKEN': 'EAAbrMRr7dV0BAA6s10xxxxxxxxxxxxnfGfGkFd9Lqx1gyqi66gZDZD',
     'VERIFY_TOKEN': 'verify_hodorbot',
-    'SERVER_URL': 'https://hodbot.herokuapp.com'
+    'SERVER_URL': 'heroku app url'
 }
